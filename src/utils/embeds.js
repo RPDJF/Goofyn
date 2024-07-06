3bef94cc7a180c5fa11fc0cf515f43e268c10e07
rpdjf <ruipaulo.unify@outlook.fr>
2024-07-07 00:35:52 +0200

    feat: Add embed utility functions

---
+++ src/utils/embeds.js
@@ -0,0 +1,41 @@
+const { EmbedBuilder, Embed } = require("discord.js");
+const { name, version, repository, logo } = require("../../package.json");
+const logger = require("./logger");
+
+class Author {
+    /**
+     * @param {string} name
+     * @param {string} iconURL
+     * @param {string} url
+     */
+    constructor(name, iconURL, url) {
+        this.name = name;
+        this.iconURL = iconURL;
+        this.url = url;
+    }
+}
+
+const botAuthor = new Author(name.charAt(0).toUpperCase() + name.slice(1), logo.url, repository.url.split('+')[1]);
+
+function msg(title, description, color = 0x7289DA, author = botAuthor) {
+    const embed = new EmbedBuilder()
+        .setColor(color)
+        .setTitle(title)
+        .setDescription(description)
+        .setAuthor(author)
+        .setFooter({
+            text: `${name} v${version}`,
+            iconURL: logo.url,
+        });
+    return embed;
+}
+
+function errorMsg(title, description, author = botAuthor) {
+    return msg(title, description, 0xFF0000, author);
+}
+
+module.exports = {
+    Author,
+    msg,
+    errorMsg,
+}
